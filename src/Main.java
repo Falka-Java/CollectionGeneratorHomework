01d8e78f529857d404d85a4d2673bf4f3b688f0e
Vladyslav <vladislavshvetsua@gmail.com>
2023-02-04 11:27:44 +0100

    Release

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
     private static final Random rn = new Random();
+
     public static void main(String[] args) {
         System.out.print("Enter collection size: ");
         int size = Integer.parseInt(sc.nextLine());
@@ -15,11 +17,35 @@ public class Main {
         int maxNum = Integer.parseInt(sc.nextLine());
 
         int[] numbers = new int[size];
-        for(int i = 0; i < numbers.length; i++){
-            numbers[i] = rn.nextInt(minNum,maxNum);
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rn.nextInt(minNum, maxNum);
         }
-        for (int i: numbers) {
+
+        //Sorting using default algorithm
+        //Arrays.sort(numbers);
+
+        //Sorting using Bubble sorting
+         BubbleSort(numbers);
+
+        for (int i : numbers) {
             System.out.print(i + " ");
         }
     }
+
+    private static void BubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            boolean isSwapped = false;
+            for (int x = 0; x < arr.length-1; x++){
+                if(arr[x] > arr[x+1]){
+                    int temp = arr[x];
+                    arr[x] = arr[x+1];
+                    arr[x+1]=temp;
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped)
+                break;
+        }
+    }
+
 }
